06c222026ebf4364f760e95bac23b1b90ac8ea4f
Rapha-Morette <raphaelmrtt14@gmail.com>
2023-06-22 14:33:11 -0300

    Desafio concluído

--- src/br/com/alura/minhasmusicas/modelos/Audio.java
+++ src/br/com/alura/minhasmusicas/modelos/Audio.java
@@ -1,2 +1,37 @@
-package br.com.alura.minhasmusicas.modelos;public class Audio {
+package br.com.alura.minhasmusicas.modelos;
+
+public class Audio {
+    private String titulo;
+    private int totalReproducoes;
+    private int totalCurtidas;
+    private int classificação;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getTotalReproducoes() {
+        return totalReproducoes;
+    }
+
+    public int getTotalCurtidas() {
+        return totalCurtidas;
+    }
+
+    public int getClassificação() {
+        return classificação;
+    }
+
+    public void curte() {
+        totalCurtidas ++;
+    }
+
+    public void reproduz() {
+//        System.out.println("Reproduzindo...");
+        totalReproducoes ++;
+    }
 }
